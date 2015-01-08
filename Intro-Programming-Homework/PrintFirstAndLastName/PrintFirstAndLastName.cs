589d58868e75c2c606edffc5bbe1bc05ce2b89ba
thrusanova <thrusanova@gmail.com>
2015-01-08 21:56:20 +0200

    add

---
+++ Intro-Programming-Homework/PrintFirstAndLastName/PrintFirstAndLastName.cs
@@ -0,0 +1,11 @@
+﻿//Create console application that prints your first and last name, each at a separate line.
+using System;
+class PrintFirstAndLastName
+    {
+        static void Main()
+        {
+            Console.WriteLine("Таня");
+            Console.WriteLine("Янева");
+        }
+    }
+
---
+++ Intro-Programming-Homework/SquareRoot/PrintMyName/PrintMyName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class PrintMyName
+    {
+        static void Main()
+        {
+            Console.WriteLine("My name is Tanya.");
+        }
+    }
+}
